3c8936d6e807527a01a8c072974415cc6531b051
FABIO MENINI CARDOSO DOS SANTOS <fabio.mcsantos@senacsp.edu.br>
2023-08-22 22:11:12 -0300

    update page receitaAberta

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,55 @@ function App() {
 
   return (
     <div className={styles.Root}>
-    <header className={styles.Header}>
-    <a href="#" ><img className={styles.Logo} src="./public/images/logo_cooking_sombra.png" /> </a>
-       <div className={styles.Conteudo}>  
-         <form className={styles.Form} name='search'>
-           <input className={styles.Input} type='search' name='pesquisar' value="" placeholder='pesquisar'/>
-         </form>
-         <a href="#"><button className={styles.Button}>Receitas Salgadas</button></a>
-         <a href="#"><button className={styles.Button}>Receitas Doces</button></a>
-         <a href="#"><button className={styles.Button}>Receitas Bebidas</button></a>
-         </div>
-      <div className={styles.Links}>
-        <a href="#" className={styles.Login}><img className={styles.Icons} src="./public/icons/user.png" alt="" />login / cadastrar</a>
-      </div>
-    </header>
+   
+   <div className={styles.geral}>
+      
+        <div className={styles.conteudo}>
+            <img className={styles.foto} src="./public/images/ossobuco.jpeg" alt=""/>
+            <div className={styles.receita}> 
+                    <h2 className={styles.TitleH2}>Ingredientes</h2>
+                        <ul className={styles.TextUl} >
+                          <li>2 colheres de sopa de azeite extra virgem</li>
+                          <li> 2 kg de ossobuco fresco (não usar carne congelada)</li>
+                          <li>2 cebolas médias em rodelas</li>
+                          <li> 1 pimentão verde ou vermelho</li>
+                          <li> 1 colher de sopa de alho picado ou triturado</li>
+                          <li>4 tomates picados e sem sementes</li>
+                          <li>1 lata (340 g) de molho de tomate pronto (não usar extrato de tomate)</li>
+                          <li>1 e 1/2 colheres de sálvia desidratada (pode ser louro também)</li>
+                          <li>pimenta-do-reino branca a gosto</li>
+                          <li> sal a gosto</li>
+                        </ul>
+               
+            </div>
+        </div>
+       
+       
+       <div className={styles.conteudo}>
+           <img className={styles.Img} src="./public/images/video.jpeg" alt=""/>
+           <div className={styles.preparo}>
+                <h2 className={styles.TitleH2}>Preparo</h2>
+       
+                   <p className={styles.TitleP}>
+                       1. O prato deverá ser feito diretamente em uma panela de pressão, onde adiciona-se inicialmente o azeite. <br/>
+
+                       2. Os ingredientes deverão ser todos divididos em 2 porções e colocados em camadas na seguinte ordem: 
+                       ossobuco, cebola, alho, tomate, pimentão, molho de tomate, temperos (sálvia, pimenta-do-reino branca e sal). Repetir esta ordem novamente. <br/>
+
+                       3. Fechar a panela e levar ao fogo alto até o início da pressão (panela começa a chiar). <br/>
+
+                       4. Abaixar o fogo e cozinhar por 50 a 60 minutos (cuidado para não ultrapassar este tempo em demasia). <br/>
+
+                       5. Desligar o fogo e abrir a panela somente após a saída natural da pressão (cerca de 15 minutos). <br/>
+
+                       6. O ideal é servir o ossobuco acompanhado de arroz branco e uma bela polenta ou purê de batata. <br/>
+
+                    </p>
+           </div>
+       </div>
+   </div>
    </div>
  )
 }
 
 export default App
-
